59077a4f05c0239b81423a588cb1a17d396744a8
vicky <vickykumarsunilja@gmail.com>
2024-04-29 21:08:19 +0530

    fixed notification bug

--- src/graphql/resolvers/currentUserRestaurant.ts
+++ src/graphql/resolvers/currentUserRestaurant.ts
@@ -205,7 +205,10 @@ export const restaurant = {
       }
       order.status = status;
       await order.save();
-      await createNotification(`Order Update: ${order.status}`, req.userId);
+      await createNotification(
+        `Order Update: ${order.status}`,
+        order.user._id.toString()
+      );
       if (order.status === "delivered") {
         await createNotification(
           "Your order has been delivered",
